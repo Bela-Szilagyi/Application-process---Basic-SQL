404df17587e524147c98fef3544c055a8f872e3b
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-05-08 21:25:00 +0200

    3. question implemented

--- queries.py
+++ queries.py
@@ -35,7 +35,20 @@ def get_nicknames():
 
 
 def get_full_name_and_phone_from_fist_name():
-    pass
+    try:
+        connect_str = "dbname='en' user='en' host='localhost'"
+        conn = psycopg2.connect(connect_str)
+        conn.autocommit = True
+        cursor = conn.cursor()
+        cursor.execute("""SELECT CONCAT (first_name, ' ', last_name) AS "full_name", phone_number
+                       FROM applicants WHERE first_name='Carol';""")
+        rows = cursor.fetchall()
+        ui.print_result(rows, 'Applicant data about Carol in 2 columns: full_name, phone_number')
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+        print(e)
+    finally:
+        return
 
 
 def get_applicant_from_e_mail():

e37ecfa8ecefd1ea99c6a6b910d54ff186f8ce39
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-05-09 10:42:33 +0200

    connection initializing and closing at all questions

--- queries.py
+++ queries.py
@@ -28,112 +28,75 @@ def get_name_columns():
 
 
 def get_nicknames():
-    try:
-        connect_str = "dbname='en' user='en' host='localhost'"
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute("""SELECT nick_name FROM mentors WHERE city='Miskolc';""")
-        rows = cursor.fetchall()
-        column_names = [desc[0] for desc in cursor.description]
-        ui.print_result(column_names, rows, 'The nick_name-s of all mentors working at Miskolc')
-    except Exception as e:
-        print("Uh oh, can't connect. Invalid dbname, user or password?")
-        print(e)
-    finally:
-        return
+    conn = init()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT nick_name FROM mentors WHERE city='Miskolc';""")
+    rows = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]
+    ui.print_result(column_names, rows, 'The nick_name-s of all mentors working at Miskolc')
+    cursor.close()
+    conn.close()
+    return
 
 
 def get_full_name_and_phone_from_fist_name():
-    try:
-        connect_str = "dbname='en' user='en' host='localhost'"
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute("""SELECT CONCAT (first_name, ' ', last_name) AS "full_name", phone_number
-                        FROM applicants WHERE first_name='Carol';""")
-        rows = cursor.fetchall()
-        column_names = [desc[0] for desc in cursor.description]
-        ui.print_result(column_names, rows, 'Applicant data about Carol in 2 columns: full_name, phone_number')
-    except Exception as e:
-        print("Uh oh, can't connect. Invalid dbname, user or password?")
-        print(e)
-    finally:
-        return
+    conn = init()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT CONCAT (first_name, ' ', last_name) AS "full_name", phone_number
+                    FROM applicants WHERE first_name='Carol';""")
+    rows = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]
+    ui.print_result(column_names, rows, 'Applicant data about Carol in 2 columns: full_name, phone_number')
+    cursor.close()
+    conn.close()
+    return
 
 
 def get_applicant_from_e_mail():
-    try:
-        connect_str = "dbname='en' user='en' host='localhost'"
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute("""SELECT CONCAT (first_name, ' ', last_name) AS "full_name", phone_number
-                       FROM applicants WHERE email LIKE '%@adipiscingenimmi.edu';""")
-        rows = cursor.fetchall()
-        column_names = [desc[0] for desc in cursor.description]
-        ui.print_result(column_names, rows, 'Applicant data with given e-mail address')
-    except Exception as e:
-        print("Uh oh, can't connect. Invalid dbname, user or password?")
-        print(e)
-    finally:
-        return
+    conn = init()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT CONCAT (first_name, ' ', last_name) AS "full_name", phone_number
+                    FROM applicants WHERE email LIKE '%@adipiscingenimmi.edu';""")
+    rows = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]
+    ui.print_result(column_names, rows, 'Applicant data with given e-mail address')
+    cursor.close()
+    conn.close()
+    return
 
 
 def get_inserted_applicant_data():
-    try:
-        connect_str = "dbname='en' user='en' host='localhost'"
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        SQL = "INSERT INTO applicants (first_name, last_name, phone_number, email, application_code) VALUES (%s, %s, %s, %s, %s);" # Note: no quotes
-        data = ("Markus", "Schaffarzyk", "003620/725-2666", "djnovus@groovecoverage.com", "54823", )
-        cursor.execute(SQL, data)
-        cursor.execute("""SELECT * FROM applicants WHERE application_code='54823';""")
-        rows = cursor.fetchall()
-        column_names = [desc[0] for desc in cursor.description]
-        ui.print_result(column_names, rows, 'Applicant data after inserting it')
-    except Exception as e:
-        print("Uh oh, can't connect. Invalid dbname, user or password?")
-        print(e)
-    finally:
-        return
+    conn = init()
+    cursor = conn.cursor()
+    SQL = "INSERT INTO applicants (first_name, last_name, phone_number, email, application_code) VALUES (%s, %s, %s, %s, %s);" # Note: no quotes
+    data = ("Markus", "Schaffarzyk", "003620/725-2666", "djnovus@groovecoverage.com", "54823", )
+    cursor.execute(SQL, data)
+    cursor.execute("""SELECT * FROM applicants WHERE application_code='54823';""")
+    rows = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]
+    ui.print_result(column_names, rows, 'Applicant data after inserting it')
+    cursor.close()
+    conn.close()
+    return
 
 
 def get_updated_applicant_phone():
-    try:
-        connect_str = "dbname='en' user='en' host='localhost'"
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute("""UPDATE applicants SET phone_number = '003670/223-7459' WHERE first_name='Jemima' AND last_name='Foreman';""")
-        cursor.execute("""SELECT phone_number FROM applicants WHERE first_name='Jemima' AND last_name='Foreman';""")
-        rows = cursor.fetchall()
-        column_names = [desc[0] for desc in cursor.description]
-        ui.print_result(column_names, rows, 'Applicant data after inserting it')
-    except Exception as e:
-        print("Uh oh, can't connect. Invalid dbname, user or password?")
-        print(e)
-    finally:
-        return
+    conn = init()
+    cursor = conn.cursor()
+    cursor.execute("""UPDATE applicants SET phone_number = '003670/223-7459' WHERE first_name='Jemima' AND last_name='Foreman';""")
+    cursor.execute("""SELECT phone_number FROM applicants WHERE first_name='Jemima' AND last_name='Foreman';""")
+    rows = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]
+    ui.print_result(column_names, rows, 'Applicant data after inserting it')
+    cursor.close()
+    conn.close()
+    return
 
 
 def get_remove_applicants_by_e_mail():
-    try:
-        connect_str = "dbname='en' user='en' host='localhost'"
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute("""DELETE FROM applicants WHERE email LIKE '%mauriseu.net';""")
-    except Exception as e:
-        print("Uh oh, can't connect. Invalid dbname, user or password?")
-        print(e)
-    finally:
-        return
-
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
+    conn = init()
+    cursor = conn.cursor()
+    cursor.execute("""DELETE FROM applicants WHERE email LIKE '%mauriseu.net';""")
+    cursor.close()
+    conn.close()
+    return

abd1fd1cee7d601fc078805fbaaa5e72716d50ba
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-05-26 21:03:56 +0200

    modified error handling, init:db in separate function

--- data_manager.py
+++ data_manager.py
@@ -1,23 +1,36 @@
 import psycopg2
-import sys
 
 
 def handle_database(command):
     result = {}
-    result['error'] = 'No error'
+    connect_str = "dbname='en' user='en' host='localhost'"
+    connection = init_db(connect_str)
+    if connection == 'connection error':
+        result['result'] = 'Connection error. Server can not make a connection.'
+        return result
+    else:
+        try:
+            connection.autocommit = True
+            cursor = connection.cursor()
+            cursor.execute(command)
+            if "SELECT" in command:
+                result['rows'] = cursor.fetchall()
+                result['column_names'] = [desc[0] for desc in cursor.description]
+                result['row_count'] = cursor.rowcount
+            cursor.close()
+            result['result'] = 'success'
+        except Exception as e:
+            result['result'] = e
+            print(e)
+        finally:
+            return result
+
+
+def init_db(connection_data):
+    conn = None
     try:
-        connect_str = "dbname='en' user='en' host='localhost'"
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute(command)
-        if "SELECT" in command:
-            result['rows'] = cursor.fetchall()
-            result['column_names'] = [desc[0] for desc in cursor.description]
-            result['row_count'] = cursor.rowcount
-        return result
-        cursor.close()
-        conn.close()
+        conn = psycopg2.connect(connection_data)
+        return conn
     except Exception as e:
-        result['error'] = e
-        return result
+        print(e)
+        return 'connection error'
